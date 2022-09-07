c8e6020288dfccd0e9588eb2990041d1f05542f3
AnnieNeilson <bnefnee@gmail.com>
2022-09-07 12:04:57 +0000

    use prevState and prevProps to create increment button and button that conditionally renders allowing the user to flip from initial message to new and back again.

--- my-app/src/components/StatefulGreeting.js
+++ my-app/src/components/StatefulGreeting.js
@@ -7,21 +7,38 @@ class StatefulGreeting  extends React.Component {
         this.state = {
             introduction: "Hello!",
             buttonText: "Exit",
+            count: 0,
         };
     }
 
-    handleClick() {
-        this.setState({
-            introduction: 'Goodbye!',
+    incrementClick() {
+        this.setState((prevState, prevProps) => {
+            console.log('prevstate', prevState)
+            console.log('orevprops', prevProps)
+            return{
+                count: prevState.count + 1
+            }
         });
-        console.log(this.state.introduction);
     }
+
+    handleClick() {
+        this.setState((prevState, prevProps) => {
+            console.log('Previous State: ', prevState)
+            console.log('Previous Props: ', prevProps)
+            return {
+                introduction: prevState.introduction === 'Hello!' ? 'Goodbye!' : 'Hello!',
+                buttonText: prevState.buttonText === 'Exit' ? 'Enter' : 'Exit',
+            };
+        });
+    };
 
     render() {
         return(
             <div>
                 <h1>{this.state.introduction} {this.props.greeting}</h1>
                 <button onClick={ () => this.handleClick()}>{this.state.buttonText}</button>
+                <button onClick={ ()=> this.incrementClick()}>{this.state.count}</button>
+                <p>You've clicked {this.state.count} times.</p>
             </div>
         ) 
     }
